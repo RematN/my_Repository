9997540b1934f413e01941f57bc14747c92ddac1
RematN <rehmatnoyda@gmail.com>
2023-02-06 10:21:01 +0530

    second commit

---
+++ marks.py
@@ -0,0 +1,9 @@
+
+M1 = int(input('enter maths marks :'))
+M2 = int(input('enter inglish marks :'))
+M3 = int(input('enter c++ marks:'))
+M4 = int(input('enter python marks:'))
+total = M1 + M2 + M3 + M4
+p = (total/400)*100
+print(f'total Marks = {total}')
+print(f'per = {p}')
